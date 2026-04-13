f5ce69911c05a84f92029201fcac21a2b93ce64c
EeroKoivukoski <eero.koivukoski@metropolia.fi>
2026-04-13 13:52:50 +0300

    Checkstyle ehdotus muutoksia

--- src/main/java/com/example/app/SessionManager.java
+++ src/main/java/com/example/app/SessionManager.java
@@ -1,52 +1,51 @@
 package com.example.app;
 
 public class SessionManager {
-    private static String currentUsername;
-    private static String currentEmail;
-    private static String currentToken;
-    private static String currentUserType;
-    private static Integer currentUserId;
+  private static String currentUsername;
+  private static String currentEmail;
+  private static String currentToken;
+  private static String currentUserType;
+  private static Integer currentUserId;
 
-    public static void setSession(String username, String email, String token, String userType) {
-        currentUsername = username;
-        currentEmail = email;
-        currentToken = token;
-        currentUserType = userType;
-    }
+  public static void setSession(String username, String email, String token, String userType) {
+    currentUsername = username;
+    currentEmail = email;
+    currentToken = token;
+    currentUserType = userType;
+  }
 
-    public static void setUserId(Integer userId) {
-        currentUserId = userId;
-    }
+  public static void setUserId(Integer userId) {
+    currentUserId = userId;
+  }
 
-    public static void clearSession() {
-        currentUsername = null;
-        currentEmail = null;
-        currentToken = null;
-        currentUserType = null;
-    }
+  public static void clearSession() {
+    currentUsername = null;
+    currentEmail = null;
+    currentToken = null;
+    currentUserType = null;
+  }
 
-    public static String getUsername() {
-        return currentUsername;
-    }
+  public static String getUsername() {
+    return currentUsername;
+  }
 
-    public static String getEmail() {
-        return currentEmail;
-    }
+  public static String getEmail() {
+    return currentEmail;
+  }
 
-    public static String getToken() {
-        return currentToken;
-    }
+  public static String getToken() {
+    return currentToken;
+  }
 
-    public static String getUserType() {
-        return currentUserType;
-    }
+  public static String getUserType() {
+    return currentUserType;
+  }
 
-    public static Integer getUserId() {
-        return currentUserId;
-    }
+  public static Integer getUserId() {
+    return currentUserId;
+  }
 
-    public static boolean isLoggedIn() {
-        return currentToken != null && !currentToken.isEmpty();
-    }
+  public static boolean isLoggedIn() {
+    return currentToken != null && !currentToken.isEmpty();
+  }
 }
-
